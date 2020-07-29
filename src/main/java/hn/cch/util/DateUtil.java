51964f5a0ffcd084601d12ea441289a048aaab43
hangcchhn <hangcchhn@163.com>
2020-07-29 19:14:34 +0800

    同步时间修改更新

--- src/main/java/hn/cch/util/DateUtil.java
+++ src/main/java/hn/cch/util/DateUtil.java
@@ -3,7 +3,6 @@ package hn.cch.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
